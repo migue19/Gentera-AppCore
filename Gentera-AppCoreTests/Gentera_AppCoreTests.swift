86d9ba3af909a8b30f451b5064b06ebef0a231e3
Miguel Mexicano Herrera <miguel.mexicano@ait-business.com>
2025-12-03 16:03:02 -0600

    Initial Commit

---
+++ Gentera-AppCoreTests/Gentera_AppCoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gentera_AppCoreTests.swift
+//  Gentera-AppCoreTests
+//
+//  Created by Miguel.mexicano on 03/12/25.
+//
+
+import Testing
+@testable import Gentera_AppCore
+
+struct Gentera_AppCoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Gentera-AppCoreUITests/Gentera_AppCoreUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Gentera_AppCoreUITestsLaunchTests.swift
+//  Gentera-AppCoreUITests
+//
+//  Created by Miguel.mexicano on 03/12/25.
+//
+
+import XCTest
+
+final class Gentera_AppCoreUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
